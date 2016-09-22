a316b84be2d07bd7b4bc8ba815bce9aef04b2a62
Brian Taylor <c.brian.taylor@gmail.com>
2016-09-22 11:13:13 -0700

    worked on assignment one files

---
+++ a1/multihistogram.m
@@ -0,0 +1,31 @@
+h1 = histogram(X(:,1))
+h1.BinWidth = 0.2
+
+hold on
+
+h2 = histogram(X(:,2))
+h2.BinWidth = 0.2
+
+h3 = histogram(X(:,3))
+h3.BinWidth = 0.2
+
+h4 = histogram(X(:,4))
+h4.BinWidth = 0.2
+
+h5 = histogram(X(:,5))
+h5.BinWidth = 0.2
+
+h6 = histogram(X(:,6))
+h6.BinWidth = 0.2
+
+h7 = histogram(X(:,7))
+h7.BinWidth = 0.2
+
+h8 = histogram(X(:,8))
+h8.BinWidth = 0.2
+
+h9 = histogram(X(:,9))
+h9.BinWidth = 0.2
+
+h10 = histogram(X(:,10))
+h10.BinWidth = 0.2
